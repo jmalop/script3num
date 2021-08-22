58c498a76dae69da0e40b3e4d3ec341489283e1e
jottama <4325925+jottama@users.noreply.github.com>
2021-08-22 13:09:18 +0200

    Update b4sc4n.sh

--- b4sc4n.sh
+++ b4sc4n.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #for PORT in {1..65535}; do (echo > /dev/tcp/X.X.X.X/$PORT) >/dev/null 2>&1 && echo $PORT is open; done
 #for PORT in {1..65535}; do timeout 1 bash -c "</dev/tcp/X.X.X.X/$PORT >/dev/null" && echo "port $PORT is open"; done
 
