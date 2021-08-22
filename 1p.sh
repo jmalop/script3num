2de433bae64b268a69beb6b62c38c470df4adc46
jottama <4325925+jottama@users.noreply.github.com>
2021-08-22 17:14:25 +0200

    Update 1p.sh

--- 1p.sh
+++ 1p.sh
@@ -192,7 +192,7 @@ cat results
 
 echo -e "\n\nRunning Nmap UDP...\n"
 #nmap -n --top-ports 2000 --reason --min-rate 3000 -sU $1 | tail -n +6 >> temp3
-nmap -sU -p- --max-rtt-timeout 10 --max-scan-delay 2 --min-hostgroup 10 &1 -n -Pn >> temp3
+nmap -sU --top-ports 15000 --max-rtt-timeout 10 --max-scan-delay 2 --min-hostgroup 10 --reason --min-rate 5000 10.10.11.100 -n  | tail -n +6 | head -n -1 >> temp3
 
 #sudo nmap -sU -p- --open -sV --version-intensity 1 $1 -n -Pn
 #sudo nmap -sU -p- --open $1 -n -Pn
