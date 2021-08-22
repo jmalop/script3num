52fda26103f764201e7a50aafbe070b66115ef7e
jottama <4325925+jottama@users.noreply.github.com>
2021-08-22 10:09:12 +0200

    Update ip3num.sh

--- ip3num.sh
+++ ip3num.sh
@@ -15,7 +15,8 @@ printf "\n\n------ NMAP ------\n\n" > results
 echo -e "\n\nRunning Nmap TCP...\n"
 #nmap --top-ports 10000 --open -sS --min-rate 5000 $1 | tail -n +5 | head -n -1 >> results 
 #nmap -sS --top-ports 10000 --open -sV -n --min-rate 5000 $1 | tail -n +6 | head -n -4 >> results
-nmap -p- --open -sS --min-rate 5000 $1 -n -Pn | tail -n +6 | head -n -4 >> results
+#nmap -p- --open -sS --min-rate 5000 $1 -n -Pn | tail -n +6 | head -n -4 >> results
+nmap -p- -sS --min-rate 5000 $1 -n -Pn | tail -n +6 | head -n -1 >> results
 
 ###ToOptimize
 #ports=$(cat results | awk '{print $1}' | sed 's/\/tcp/,/g' | tr -d "\n" | sed '$ s/.$//' | sed 's/------PORT//g' | sed 's/,Nma//g' | sed 's/------Some//g' | sed 's/PORT//g' | sed 's/,Servic//g'  | sed 's/------//g')
